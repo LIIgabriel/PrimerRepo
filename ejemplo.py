2f389507b32fb3fd04572cec5111ba40b28b865e
Juan Gabriel Lopez Solorzano <juangabriel.lopez@gmail.com>
2020-07-22 18:33:33 -0500

    add ejemplo

---
+++ ejemplo.py
@@ -0,0 +1,23 @@
+class Speed:
+    SLOW = 0.3
+    NORMAL = 1
+
+class gTTS:
+    def __init__():
+      print("dlldld")
+    
+    def _tokenize(self, text):
+        #codigo aqui
+
+    
+    def _prepare_requests(self):
+        print("dldlldldld")
+
+    def get_urls(self):
+        #codigo aqui
+
+    def write_to_fp(self, fp):
+       #codigo aqui
+
+    def save(self, savefile):
+        #codigo aqui
